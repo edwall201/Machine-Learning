1a14bb3aad20d026eb10af69df1705756c758410
YU-KAI HUANG <edwall201@gmail.com>
2024-03-31 14:53:29 +0800

    HDBScan algo practice

---
+++ HDBSCAN.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import make_blobs
+import hdbscan
+import matplotlib.pyplot as plt
+
+N_SAMPLES = 1000
+RANDOM_STATE = 42
+X, y = make_blobs(n_samples=N_SAMPLES,
+                  cluster_std=[2.0, 0.5],
+                  centers=[(0, 0), (5, 5)],
+                  random_state=RANDOM_STATE)
+
+plt.figure(figsize = (10, 10))
+plt.scatter(X[:, 0], X[:, 1])
+plt.show()
+
+hclusterer = hdbscan.HDBSCAN(min_cluster_size=5).fit(X)
+
+plt.figure(figsize = (10, 10))
+plt.scatter(X[:, 0], X[:, 1], c = hclusterer.labels_)
+plt.show()
